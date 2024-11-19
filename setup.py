188a8afef586b2172274a396e00d530a66dd1112
atroriusss <artorius.ua@gmail.com>
2024-11-19 15:38:36 +0200

    +setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="satellite-imagery-downloader",
+    version="0.1.0",
+    description="A tool for downloading satellite imagery",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/artoriusss/satellite-imagery-downloader",
+    packages=find_packages(), 
+    install_requires=[
+        "numpy>=1.23.2",
+        "opencv_python>=4.6.0.66",
+        "requests>=2.28.1"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
