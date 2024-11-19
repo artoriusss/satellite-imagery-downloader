dcb0d9a2395d6824657b6c29c9bd6de90577eb6f
atroriusss <artorius.ua@gmail.com>
2024-11-19 15:19:35 +0200

    modularire & refactor

--- src/main.py
+++ src/main.py
@@ -4,33 +4,9 @@ import re
 import cv2
 from datetime import datetime
 
+from prefs import file_dir, prefs_path, default_prefs
 from image_downloading import download_image
 from adjust_resolution import adjust_for_resolution
-
-file_dir = os.path.dirname(__file__)
-prefs_path = os.path.join(file_dir, 'preferences.json')
-default_prefs = {
-        'url': 'https://mt.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
-        'tile_size': 256,
-        'channels': 3,
-        'dir': os.path.join(file_dir, 'images'),
-        'headers': {
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36'
-        },
-        'tl': '',
-        'br': '',
-        'zoom': ''
-    }
-
 
 def take_input(messages):
     inputs = []
@@ -43,7 +19,6 @@ def take_input(messages):
             return take_input(messages)
         inputs.append(inp)
     return inputs
-
 
 def run():
     with open(os.path.join(file_dir, 'preferences.json'), 'r', encoding='utf-8') as f:
@@ -79,11 +54,5 @@ def run():
     cv2.imwrite(os.path.join(prefs['dir'], name), img)
     print(f'Saved as {name}')
 
-
 if os.path.isfile(prefs_path):
     run()
-else:
-    with open(prefs_path, 'w', encoding='utf-8') as f:
-        json.dump(default_prefs, f, indent=2, ensure_ascii=False)
-
-    print(f'Preferences file created in {prefs_path}')
